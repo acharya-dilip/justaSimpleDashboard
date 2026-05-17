f864a31a3acd56a5035cf091bc100c8adae16504
acharya-dilip <acharyamission6@gmail.com>
2026-05-17 11:13:17 +0545

    added some nav links in the header

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<img src="https://cdn.hackclub.com/019e343b-acbc-772d-8eba-872f7554f5d3/prism_launcher_logo.svg" alt="Logo" {{ $attributes }} />
---
+++ resources/views/meeting.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h1 class="font-semibold text-7xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Meeting') }}
+        </h1>
+    </x-slot>
+
+
+</x-app-layout>
