c509571649f37ac6ab7b3d37e95d213e0f7d175e
acharya-dilip <acharyamission6@gmail.com>
2026-05-17 13:56:13 +0545

    wrote the controller for handling the post creation inside the database

--- app/Http/Controllers/dashboard/postController.php
+++ app/Http/Controllers/dashboard/postController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 
 class postController extends Controller
 {
@@ -18,7 +19,12 @@ class postController extends Controller
                $request->file('image')->storeAs('public/uploads', $filename);
         }
 
+        $post = new post;
 
+        $post['description']->storeAs($validate['description']);
+        $post['image_path']->storeAs($filename);
+
+        return redirect(route('dashboard'));
 
     }
 
