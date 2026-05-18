cd3822bda7532117ac51266ea592aaa778882f46
acharya-dilip <acharyamission6@gmail.com>
2026-05-18 12:13:15 +0545

    table creation with tailwind??

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -9,7 +9,7 @@
     <x-container class="my-10" style="max-width: 65%">
         <h1 class="text-3xl font-bold">Member Enrollment</h1>
 
-        <div class="grid grid-cols-1 grid-cols-2 grid-cols-3 grid-cols-4 grid-cols-5 grid-cols-6 grid-cols-7 grid-cols-8 grid-cols-9 grid-cols-10 grid-cols-11">
+        <div class="grid border text-lg grid-cols-1 grid-cols-2 grid-cols-3 grid-cols-4 grid-cols-5 grid-cols-6 grid-cols-7 grid-cols-8 grid-cols-9 grid-cols-10 grid-cols-11">
 
             <div>{{--For the image--}}
                 PICTURE
