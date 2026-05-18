680ed8a969e4c85396a677517dada029721eea86
acharya-dilip <acharyamission6@gmail.com>
2026-05-18 09:41:25 +0545

    removed id from fillables

--- app/Models/member.php
+++ app/Models/member.php
@@ -9,7 +9,7 @@ class member extends Model
     //
     protected $table = 'members';
 
-    protected $fillable = ['id','image','name','email','number','tech stack','role'];
+    protected $fillable = ['image','name','email','number','tech stack','role'];
 
      protected $primaryKey = 'id';
 
