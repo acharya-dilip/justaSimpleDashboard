ffbd5ae219773e07179c3484ca5bcc1456e87309
acharya-dilip <acharyamission6@gmail.com>
2026-05-18 16:41:32 +0545

    wrote the create method in the meeting controller to create and store meetings in the db

--- app/Http/Controllers/meeting/meetingController.php
+++ app/Http/Controllers/meeting/meetingController.php
@@ -3,12 +3,31 @@
 namespace App\Http\Controllers\meeting;
 
 use App\Http\Controllers\Controller;
+use App\Models\meeting;
+use Illuminate\Http\Request;
 
 class meetingController extends Controller
 {
-        function create($request){
+        function create(Request $request){
 
+            $validate = request()->validate([
+                'agenda' => 'required',
+                'time' => 'required',
+                'location' => 'required',
+                'context' => 'required',
+                'locationLink' => 'required',
 
+            ]);
+
+            $meeting = new meeting();
+
+            $meeting->agenda = $request->agenda;
+            $meeting->time = $request->time;
+            $meeting->location = $request->location;
+            $meeting->context = $request->context;
+            $meeting->locationLink = $request->locationLink;
+
+            $meeting->save();
 
         }
 }
