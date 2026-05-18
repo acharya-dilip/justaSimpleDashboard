98bd985938d1db9a8ffc3fe9982e84bdbf5d947b
acharya-dilip <acharyamission6@gmail.com>
2026-05-18 13:52:50 +0545

    updated the User model to make id the primary key

--- app/Models/User.php
+++ app/Models/User.php
@@ -34,6 +34,7 @@ class User extends Authenticatable
     protected $table = 'users';
     protected $fillable = ['name', 'email', 'role','id'];
 
+    protected $primaryKey = ['id'];
     use HasFactory;
 
 }
